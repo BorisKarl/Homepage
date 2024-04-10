8725c23ba5f0ab855850c576ed9a5dd1ad304a43
BorisKarl <herrnielsen@web.de>
2024-04-10 10:35:00 +0200

    changed footer pic/rotate, changed bio

--- src/sript.js
+++ src/sript.js
@@ -13,31 +13,8 @@ links.forEach((link) => {
                 target.scrollIntoView({behavior: "smooth",})
     })
 })
-/*
-const navList = document.querySelector(".nav_list");
-navList.addEventListener("click", () => {
-    let check = document.getElementById("top_link");
-    console.log(check);
-    if(check) return;
-    else {
-         const top = document.createElement("a");
-         top.setAttribute("id", "top_link")
-         top.setAttribute("class", "sectionLink");
-         top.setAttribute("href", "#top");
-         top.textContent = "Top";
-         navList.insertBefore(top, navList.firstChild);
-         top.addEventListener("click", (e) => {
-           e.preventDefault();
 
-           let tmp = top.getAttribute("href");
-           let id = tmp.substring(1);
-           let target = document.getElementById(id);
-           console.log(target);
-           top.scrollIntoView({ behavior: "smooth" });
-         });
-    }
-   
+const pic = document.getElementById("footer_pic");
+pic.addEventListener("mouseover", () => {
+    pic.classList.toggle("move");
 })
-
-*/
-
